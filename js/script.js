374c28ffcfb379ff79241ad7e23253f0ff01e2f6
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-01-30 11:13:02 -0500

    fixed all 6 media queries

--- js/script.js
+++ js/script.js
@@ -3,63 +3,67 @@ var title = '';
 var linkUrl = '';
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 	var home = "https://api.nytimes.com/svc/topstories/v2/home";
 	home += ".json";
 	home += '?' + $.param({
 		'api-key': "c0cc4db94b8348c184393e9d70096e88"
 	});
-		$.ajax({
-			url: home,
-			method: 'GET',
-		}).done(function(result) {
-			for (var i=0; i < result.results.length; i++){
-				if (result.results[i].multimedia.length != 0) {
-					title = result.results[i].title;
-					linkUrl = result.results[i].url;
-					imgUrl = result.results[i].multimedia[result.results[i].multimedia.length-1].url;
-					var article = $('<div class="article"><a href=' + linkUrl + '><p></p></a></div>');
-		
-					article.css({'background': 'url('+ imgUrl +') no-repeat center',
-								'background-size':'cover','border': 'none'});
-					article.find('p').text(title);
-					$('.display').append(article);
-					}
+	$.ajax({
+		url: home,
+		method: 'GET',
+	}).done(function (result) {
+		for (var i = 0; i < result.results.length; i++) {
+			if (result.results[i].multimedia.length != 0) {
+				title = result.results[i].title;
+				linkUrl = result.results[i].url;
+				imgUrl = result.results[i].multimedia[result.results[i].multimedia.length - 1].url;
+				var article = $('<div class="article"><a style="text-decoration: none; text-align: center;" href=' + linkUrl + '><p></p></a></div>');
+
+				article.css({
+					'background': 'url(' + imgUrl + ') no-repeat center',
+					'background-size': 'cover', 'border': 'none'
+				});
+				article.find('p').text(title);
+				$('.display').append(article);
+			}
 		};
-		}).fail(function(err) {
-			throw err;
-		});
+	}).fail(function (err) {
+		throw err;
+	});
 });
-	$('select').on('click',function(){ 
-		$('.display').empty();
-		var url = "https://api.nytimes.com/svc/topstories/v2/";
+$('select').on('click', function () {
+	$('.display').empty();
+	var url = "https://api.nytimes.com/svc/topstories/v2/";
 	url += $('select option:selected').text().toLowerCase();
 	url += ".json";
 	url += '?' + $.param({
-	  'api-key': "c0cc4db94b8348c184393e9d70096e88"
-});
+		'api-key': "c0cc4db94b8348c184393e9d70096e88"
+	});
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-	for (var i=0; i < result.results.length; i++){
-		if (result.results[i].multimedia.length != 0) {
-			title = result.results[i].title;
-			linkUrl = result.results[i].url;
-			imgUrl = result.results[i].multimedia[result.results[i].multimedia.length-1].url;
-			var article = $('<div class="article"><a href=' + linkUrl + '><p></p></a></div>');
+	$.ajax({
+		url: url,
+		method: 'GET',
+	}).done(function (result) {
+		for (var i = 0; i < result.results.length; i++) {
+			if (result.results[i].multimedia.length != 0) {
+				title = result.results[i].title;
+				linkUrl = result.results[i].url;
+				imgUrl = result.results[i].multimedia[result.results[i].multimedia.length - 1].url;
+				var article = $('<div class="article"><a style="text-decoration: none; text-align: center;" href=' + linkUrl + '><p></p></a></div>');
 
-			article.css({'background': 'url('+ imgUrl +') no-repeat center',
-					  'background-size':'cover','border': 'none'});
-			article.find('p').text(title);
-			$('.display').append(article);
+				article.css({
+					'background': 'url(' + imgUrl + ') no-repeat center',
+					'background-size': 'cover', 'border': 'none'
+				});
+				article.find('p').text(title);
+				$('.display').append(article);
 			}
-};
+		};
 
-}).fail(function(err) {
-  throw err;
-});
+	}).fail(function (err) {
+		throw err;
+	});
 
 });
 
